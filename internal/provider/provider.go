ef377f2e703c12305464f3bf167aeb7b332671c2
Matthew John <matthew@dockstudios.co.uk>
2024-01-28 07:08:54 +0000

    Remove commented code
    Issue #1

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -84,7 +84,6 @@ func (p *TerraregProvider) Configure(ctx context.Context, req provider.Configure
 
 	tflog.Debug(ctx, "Creating Terrareg client")
 
-	// client := http.DefaultClient
 	api, err := terrareg.NewClient(url, data.ApiKey.ValueString())
 	if err != nil {
 		resp.Diagnostics.AddError(
