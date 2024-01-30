e199d66ff5d19f097971934150ee36d170d513b6
Matthew John <matthew@dockstudios.co.uk>
2024-01-30 05:51:10 +0000

    test: Add tests for git_provider data source
    Issue #2

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ go test $(go list ./...) -count=1 -v
 
 To run acceptance tests, run an instance of terrareg (https://github.com/matthewjohn/terrareg) and run acceptance tests:
 ```
-docker run -d -p 5000:5000 -e MIGRATE_DATABASE=true -e ADMIN_AUTHENTICATION_TOKEN=password ghcr.io/matthewjohn/terrareg:latest
+docker run -d -p 5000:5000 -e GIT_PROVIDER_CONFIG='[{"name": "Github", "base_url": "https://github.com/{namespace}/{module}", "clone_url": "ssh://git@github.com:{namespace}/{module}.git", "browse_url": "https://github.com/{namespace}/{module}/tree/{tag}/{path}"}, {"name": "Bitbucket", "base_url": "https://bitbucket.org/{namespace}/{module}", "clone_url": "ssh://git@bitbucket.org:{namespace}/{module}-{provider}.git", "browse_url": "https://bitbucket.org/{namespace}/{module}-{provider}/src/{tag_uri_encoded}/{path}"}, {"name": "Gitlab", "base_url": "https://gitlab.com/{namespace}/{module}", "clone_url": "ssh://git@gitlab.com:{namespace}/{module}-{provider}.git", "browse_url": "https://gitlab.com/{namespace}/{module}-{provider}/-/tree/{tag}/{path}"}]' -e MIGRATE_DATABASE=true -e ADMIN_AUTHENTICATION_TOKEN=password ghcr.io/matthewjohn/terrareg:latest
 
 TF_ACC=1 TERRAREG_URL=http://localhost:5000 TERRAREG_API_KEY=password go test $(go list ./...) -count=1 -v
 ```
---
+++ internal/provider/git_providers_data_source_test.go
@@ -0,0 +1,33 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccGitProvidersDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				Config: buildTestProviderConfig(testAccGitProvidersDataSourceConfig),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.terrareg_git_providers.this", "git_providers.%", "3"),
+					resource.TestCheckResourceAttr("data.terrareg_git_providers.this", "git_providers.0.id", "1"),
+					resource.TestCheckResourceAttr("data.terrareg_git_providers.this", "git_providers.0.name", "Github"),
+					resource.TestCheckResourceAttr("data.terrareg_git_providers.this", "git_providers.1.id", "2"),
+					resource.TestCheckResourceAttr("data.terrareg_git_providers.this", "git_providers.1.name", "Bitbucket"),
+					resource.TestCheckResourceAttr("data.terrareg_git_providers.this", "git_providers.2.id", "3"),
+					resource.TestCheckResourceAttr("data.terrareg_git_providers.this", "git_providers.2.name", "Gitlab"),
+				),
+			},
+		},
+	})
+}
+
+const testAccGitProvidersDataSourceConfig = `
+data "terrareg_git_providers" "this" { }
+`
